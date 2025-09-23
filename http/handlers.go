42f0bceb0e736a4a61ebe37ca91402c5545cac28
Mikhail <skulllalka@gmail.com>
2025-09-23 16:54:47 +0300

    Created HandleCreateTask

--- http/handlers.go
+++ http/handlers.go
@@ -2,7 +2,11 @@ package http
 
 import (
 	"API/todo"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
 )
 
 type HTTPHandlers struct {
@@ -16,7 +20,43 @@ func NewHTTPHandlers(todoList *todo.List) *HTTPHandlers {
 }
 
 func (h *HTTPHandlers) HandleCreateTask(w http.ResponseWriter, r *http.Request) {
+	var taskDTO TaskDTO
+	if err := json.NewDecoder(r.Body).Decode(&taskDTO); err != nil {
+		errDTO := ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+		http.Error(w, errDTO.ToString(), http.StatusBadRequest)
+	}
+	if err := taskDTO.ValidateForCreate(); err != nil {
+		errDTO := ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+		http.Error(w, errDTO.ToString(), http.StatusBadRequest)
+	}
+	todoTask := todo.NewTask(taskDTO.Title, taskDTO.Description)
+	if err := h.todoList.AddTask(todoTask); err != nil {
+		errDTO := ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+		if errors.Is(err, todo.ErrTaskAlreadyExist) {
+			http.Error(w, errDTO.ToString(), http.StatusConflict)
+		} else {
+			http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+			return
+		}
 
+	}
+	b, err := json.MarshalIndent(w, "", "     ")
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusCreated)
+	if _, err := w.Write(b); err != nil {
+		fmt.Println("failed to write http response:", err)
+	}
 }
 
 func (h *HTTPHandlers) HandleGetTask(w http.ResponseWriter, r *http.Request) {
